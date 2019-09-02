be6580b66c41356640b3bb58c526544eed1be07f
graczek <raczek.grz@gmail.com>
2019-09-02 12:23:13 +0200

    added calculator implementation

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -10,12 +10,30 @@ public class Calculator {
         this.y = y;
     }
 
+    public Calculator() {
+
+    }
+
     public int getX() {
         return x;
     }
 
     public int getY() {
         return y;
+    }
+
+    public double add(int x, int y) {
+        return x + y;
+    }
+
+    public double substract(int x, int y) {
+        return x - y;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        double result = calculator.add(5, 6);
+        System.out.println(result);
     }
 
 
