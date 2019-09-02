c4069420751fc1d8d532eb4f9c950e1a623a0dd1
graczek <raczek.grz@gmail.com>
2019-09-02 12:15:07 +0200

    First Commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,26 @@
+package com.kodilla;
+
+public class Calculator {
+
+    private int x;
+    private int y;
+
+    public Calculator(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+
+
+
+
+
+}
